0bc45df40964a668d5df31ad6d4a18ac7cba47a0
Silas <silasandrade.code@gmail.com>
2025-11-14 21:48:45 -0300

    Adiciona modelos de usuário personalizados e estrutura inicial para o carrinho e pedidos

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,17 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     date_of_birth = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+# TODO Adicionar mais modelos relacionados a contas, como Perfil de Vendedor e Perfil de Comprador.
+
+class CustomUser(User):
+    address = ...
+    orders = ...
+
+
+class SellerUser(User):
+    name = models.CharField(max_length=25)
+    products = ...
+    links = ...
+    
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+
+#TODO Adicionar modelos relacionados ao carrinho de compras, como Item do Carrinho e Carrinho de Compras.
