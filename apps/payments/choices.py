a3d5c00fb35d1b6f8e4a1546cbc0fa1e324873bf
Silas Andrade <silasandradee@outlook.com>
2026-02-08 23:35:08 -0300

    feature: Modelagem da Entidade Payment

---
+++ apps/payments/choices.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class PaymentStatus(models.TextChoices):
+    PENDING = "pending", "Pending"
+    PAID = "paid", "Paid"
+    FAILED = "failed", "Failed"
+    REFUNDED = "refunded", "Refunded"
--- apps/payments/models.py
+++ apps/payments/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+from .choices import PaymentStatus
+from apps.orders import Order
+
+
+class Payment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    order = models.OneToOneField(
+        Order,
+        related_name="payment",
+        on_delete=models.PROTECT
+    )
+
+    provider = models.CharField(max_length=50)  # stripe, mercadopago, pix
+    provider_payment_id = models.CharField(max_length=255, blank=True)
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    status = models.CharField(
+        max_length=20,
+        choices=PaymentStatus.choices,
+        default=PaymentStatus.PENDING
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
