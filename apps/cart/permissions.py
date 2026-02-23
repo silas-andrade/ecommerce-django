73aba74961be6ee8a09b3f32363604005859a0cc
Silas Andrade <silasandradee@outlook.com>
2026-02-23 19:02:50 -0300

    Alteração na permissão IsOwnerCart para comparar usuário anônimos

--- apps/cart/permissions.py
+++ apps/cart/permissions.py
@@ -2,4 +2,4 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerCart(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.user.id == request.user.id
+        return (obj.user.id == request.user.id) or (obj.session_key == request.session.session_key)
