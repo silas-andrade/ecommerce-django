98e466fff105b5f29f349c78efd3b3a6d42b2a84
Silas Andrade <silasandradee@outlook.com>
2026-02-23 19:01:58 -0300

    Alteração no modelo Cart para aceitar usuários anônimos por meio de chaves de sessões, adição dos atributos created_at e update_at no modelo CartItem

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -7,22 +7,34 @@ from core import settings
 
 from core.models import UUIDModel, TimeStampedModel
 
+
 class Cart(UUIDModel, TimeStampedModel):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         related_name="cart",
-        on_delete=models.CASCADE
-        )
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    session_key = models.CharField(
+        max_length=40,
+        null=True,
+        blank=True,
+        db_index=True
+    )
 
 
-class CartItem(UUIDModel):
+class CartItem(UUIDModel, TimeStampedModel):
     cart = models.ForeignKey(
         Cart,
         related_name="items",
         on_delete=models.CASCADE
-        )
+    )
     product = models.ForeignKey(
         Product,
         on_delete=models.PROTECT
     )
-    quantity = models.PositiveIntegerField(default=1)
+    quantity = models.PositiveIntegerField(
+        default=1
+    )
